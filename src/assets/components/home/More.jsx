3d5a95a80a20c4058658ed54064e9cc3df437a41
Tesimune <Tesimune@gmail.com>
2023-01-22 06:19:14 +0100

    #

---
+++ src/assets/components/home/More.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function More() {
+  return (
+    <div>
+        <div className='grid grid-cols-2 p-32'>
+          <div className='grid gap-5 text-white'>
+            <span className='text-5xl'>Strategy, Design &</span>
+            <span className='text-5xl'>M|  </span>
+            <span className='text-xl'>Lets tell your target audience about your business, services and products.</span>
+            <img className='h-[300px] w-[500px] border-8 border-gray-400 rounded-md mt-16' src='/images/gif.jpg' alt='gif' />
+          </div>
+          <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default More
