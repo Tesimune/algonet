89e59c0d2717eb3a3075e3d90c055ade848eca42
Tesimune <Tesimune@gmail.com>
2023-01-25 08:31:15 +0100

    Router updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import './css/index.css'
 import './sass/app.scss';
 import App from './pages/App'
@@ -9,29 +9,18 @@ import Works from './pages/Works'
 import Services from './pages/Services'
 import ErrorPage from './pages/ErrorPage'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/works",
-    element: <Works />,
-  },
-  {
-    path: "/services",
-    element: <Services />,
-  },
-]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <BrowserRouter>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="about" element={<About />} />
+          <Route path="works" element={<Works />} />
+          <Route path="services" element={<Services />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
