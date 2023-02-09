8d44eee51fd88fc3ac0336fead8303b9450c570d
Tesimune <Tesimune@gmail.com>
2023-02-09 17:28:19 +0100

    Navbar path highlight

--- src/assets/layouts/Navbar.jsx
+++ src/assets/layouts/Navbar.jsx
@@ -15,6 +15,7 @@ function Navbar() {
     )
 
   }
+  
   return (
     <>
         <div className='bg-transparent flex justify-between items-center py-3 lg:py-9 px-5 lg:px-28 absolute z-50 w-full lg:max-w-[1400px]'>
@@ -55,6 +56,16 @@ export default Navbar;
 
 export function AltNavbar() {
 
+  const MyNavLink = ({href, label}) => {
+    const isActive = location.pathname == href;
+    const myStyle = 'text-red-500 border-b-2 border-red-500';
+   return  (
+    <NavLink>
+      <Link to={href} className={isActive ? myStyle : null}>{label}</Link>
+    </NavLink>
+   )
+  }
+
   function Hambuger(){
     const HambugerBar = document.querySelector('#HambugerBar')
     HambugerBar.classList.contains('hidden') ? (
@@ -75,18 +86,10 @@ export function AltNavbar() {
             <NavLink>
               <Link to='/'>Home</Link>
             </NavLink>
-            <NavLink>
-              <Link to='/about' active='/about'>About</Link>
-            </NavLink>
-            <NavLink>
-              <Link to='/works' active='/works'>Works</Link>
-            </NavLink>
-            <NavLink>
-              <Link to='/services' active='/services'>Services</Link>
-            </NavLink>
-            <NavLink>
-              <Link to='/contact' active='/contact'>Contact</Link>
-            </NavLink>
+            <MyNavLink href="/about" label="About" />
+            <MyNavLink href="/works" label="Works" />
+            <MyNavLink href="/services" label="Services" />
+            <MyNavLink href="/contact" label="Contact" />
           </div>
         </div>
         <Link to='/contact' className='bg-white border hidden lg:block border-red-600 p-3 text-red-600 hover:bg-red-500 hover:text-white  transition duration-500 ease-in-out'>
